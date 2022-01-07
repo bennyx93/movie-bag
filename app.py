5ce5fc3593f51a0a7202d8276eadb39eb9f6f9a7
Benny Chan <bennyx93@tamu.edu>
2022-01-07 10:37:31 -0600

    Implement Authorization and Authentication for POST, PUT and DELETE

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from database.db import initialize_db
 from flask_restful import Api
 from resources.routes import initialize_routes
 
 app = Flask(__name__)
+app.config.from_envvar('ENV_FILE_LOCATION')
+
 api = Api(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 app.config['MONGODB_SETTINGS'] = {
  'host': 'mongodb://localhost/movie-bag'
 }
 
 initialize_db(app)
+# https://flask.palletsprojects.com/en/2.0.x/blueprints/
+# Can use blueprint instead of Api. It is used to better organize the code.
 # app.register_blueprint(movies)
 initialize_routes(api)
 
--- resources/movies.py
+++ resources/movies.py
@@ -1,5 +1,6 @@
 from flask import Response, request
-from database.models import Movie
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from database.models import Movie, User
 from flask_restful import Resource
 
 class MoviesApi(Resource):
@@ -7,22 +8,34 @@ class MoviesApi(Resource):
     movies = Movie.objects().to_json()
     return Response(movies, mimetype="application/json", status=200)
 
-  def post(self):
+@jwt_required
+def post(self):
+    user_id = get_jwt_identity()
     body = request.get_json()
-    movie = Movie(**body).save()
+    user = User.objects.get(id=user_id)
+    movie = Movie(**body, added_by=user)
+    movie.save()
+    user.update(push__movies=movie)
+    user.save()
     id = movie.id
     return {'id': str(id)}, 200
  
 class MovieApi(Resource):
-  def put(self, id):
-    body = request.get_json()
-    Movie.objects.get(id=id).update(**body)
-    return '', 200
+    @jwt_required
+    def put(self, id):
+        user_id = get_jwt_identity()
+        movie = Movie.objects.get(id=id, added_by=user_id)
+        body = request.get_json()
+        Movie.objects.get(id=id).update(**body)
+        return '', 200
  
-  def delete(self, id):
-    movie = Movie.objects.get(id=id).delete()
-    return '', 200
+    @jwt_required
+    def delete(self, id):
+        user_id = get_jwt_identity()
+        movie = Movie.objects.get(id=id, added_by=user_id)
+        movie.delete()
+        return '', 200
 
-  def get(self, id):
-    movies = Movie.objects.get(id=id).to_json()
-    return Response(movies, mimetype="application/json", status=200)
+    def get(self, id):
+        movies = Movie.objects.get(id=id).to_json()
+        return Response(movies, mimetype="application/json", status=200)
