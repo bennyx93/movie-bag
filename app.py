023d774f26314e79afdd671bdd535ef6e69ce6df
Benny Chan <bennyx93@tamu.edu>
2022-01-07 11:53:33 -0600

    Implement reset password and forgot

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_mail import Mail
 from database.db import initialize_db
 from flask_restful import Api
-from resources.routes import initialize_routes
 from resources.errors import errors
 
 app = Flask(__name__)
 app.config.from_envvar('ENV_FILE_LOCATION')
+mail = Mail(app)
+
+# imports requiring app and mail
+from resources.routes import initialize_routes
 
 api = Api(app)
 api = Api(app, errors=errors)
@@ -23,5 +27,3 @@ initialize_db(app)
 # Can use blueprint instead of Api. It is used to better organize the code.
 # app.register_blueprint(movies)
 initialize_routes(api)
-
-app.run(debug=True)
--- resources/errors.py
+++ resources/errors.py
@@ -22,6 +22,12 @@ class EmailAlreadyExistsError(Exception):
 class UnauthorizedError(Exception):
     pass
 
+class EmailDoesnotExistsError(Exception):
+    pass
+
+class BadTokenError(Exception):
+    pass
+
 errors = {
     "InternalServerError": {
         "message": "Something went wrong",
@@ -54,5 +60,13 @@ errors = {
      "UnauthorizedError": {
          "message": "Invalid username or password",
          "status": 401
+     },
+     "EmailDoesnotExistsError": {
+         "message": "Couldn't find the user with given email address",
+         "status": 400
+     },
+     "BadTokenError": {
+         "message": "Invalid token",
+         "status": 403
      }
 }
